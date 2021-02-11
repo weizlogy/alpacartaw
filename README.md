3dffdcf13d3c41c051e8dd68820c069efd2778e1
weizlogy <dreamfragment623@gmail.com>
2021-02-12 05:56:52 +0900

    v1.0.0

---
+++ README.md
@@ -0,0 +1,164 @@
+# GRIMSLIME
+
+Generates Real-tIMe Subtitles in multiple Languages ​​from voice recognItion and reads theM aloud. (with OBS and obs-websockEt
+
+音声認識からリアルタイム字幕を多言語翻訳で生成し、音声合成で読み上げる。
+
+OBS連携することで、音声認識した文字列と翻訳した文字列をOBS上に表示する。
+
+# SpeechRecognition
+
+## Start
+
+音声認識を開始する。
+
+# Speech translate settings
+
+## GAS deploy key
+
+翻訳機能を持つgoogle apps scriptのデプロイキーを設定する。
+
+デフォルト：アルパカさんの
+
+落ちてる場合は自分で用意すること。
+
+``` js
+// サンプル
+function doGet(e) {
+  var p = e.parameter;
+  var translatedText = LanguageApp.translate(p.text, p.source, p.target);
+  return ContentService.createTextOutput(e.parameter.callback + '({"translated" : "' + translatedText + '"});').setMimeType(ContentService.MimeType.JAVASCRIPT);
+}
+```
+
+## NativeLang
+
+母国語を設定する。
+
+デフォルト：ja
+
+## ForeignLang
+
+翻訳対象の言語を設定する。
+
+デフォルト：en
+
+## Test
+
+翻訳をテスト実行する。
+
+テスト文字列は日本語なので、NativeLangには"ja"を設定しておくこと。
+
+# Speech voice settings
+
+## Native voice
+
+母国語の音声を設定する。
+
+デフォルト：取得可能な音声一覧の先頭
+
+## Volume
+
+母国語の音声の音量を設定する。
+
+デフォルト：1
+
+## Pitch
+
+母国語の音声の音程を設定する。
+
+デフォルト：1
+
+## Rate
+
+母国語の音声の速度を設定する。
+
+デフォルト：1
+
+## Test
+
+母国語で音声合成をテスト実行する。
+
+## Foreign voice
+
+翻訳対象の言語の音声を設定する。
+
+デフォルト：取得可能な音声一覧の先頭
+
+## Volume
+
+翻訳対象の言語の音声の音量を設定する。
+
+デフォルト：1
+
+## Pitch
+
+翻訳対象の言語の音声の音程を設定する。
+
+デフォルト：1
+
+## Rate
+
+翻訳対象の言語の音声の速度を設定する。
+
+デフォルト：1
+
+## Test
+
+翻訳対象の言語で音声合成をテスト実行する。
+
+# OBS connection settings
+
+[OBS](https://obsproject.com/ja)と[OBS-Websocket](https://github.com/Palakis/obs-websocket)の設定をしておくこと。
+
+## 動作確認環境
+
+OBS：26.1.1
+
+OBS-Websocket：4.9.0
+
+## IPAddr
+
+OBSがインストールされているPCのIPv4アドレスを設定する。
+
+デフォルト：localhost
+
+## Port
+
+OBS-Websocketで設定したポート番号を指定する。
+
+デフォルト：4444
+
+## Password
+
+OBS-Websocketで設定したパスワードを指定する。
+
+## Connect
+
+OBSに接続する。状態はボタン右に表示される。
+
+- OPEN
+
+OBSとの接続を開始した。
+
+- CONNECTED
+
+OBSとの接続が完了した。
+
+- ERROR
+
+OBSとの接続に失敗した。
+
+# OBS TextGDIPlus settings
+
+## NativeSource
+
+母国語表示用のOBSのテキストソース名を設定する。
+
+デフォルト：Native
+
+## ForeignSource
+
+翻訳対象の言語のOBSのテキストソース名を設定する。
+
+デフォルト：Native
---
+++ index.js
@@ -0,0 +1,206 @@
+// Chrome対応らしい
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var socket = null;
+
+window.onload = function() {
+  console.log('loaded.');
+  // 音声合成のVOICE一覧生成
+  speechSynthesis.onvoiceschanged = () => {
+    const createVoiceList = function(target) {
+      const selectbox = document.querySelector(target)
+      const voices = speechSynthesis.getVoices();
+      for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement('option');
+        option.setAttribute('value', i);
+        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+        selectbox.appendChild(option);
+      }
+    }
+    createVoiceList('select[name="voice-target-native"]');
+    createVoiceList('select[name="voice-target-foreign"]');
+  };
+  new SpeechSynthesisUtterance();  // これでonvoiceschangedを発火する
+
+  // OBSのいろいろ
+  document.querySelector('div[name="obs-submit"]').onclick = function() {
+    console.log('obs-submitted.');
+    // ステータス表示領域
+    const status = document.querySelector('span[name="obs-status"]');
+    status.textContent = '';
+    // 接続情報取得
+    const ipaddr = document.querySelector('input[name="obs-addr"]').value || 'localhost'
+    const port = document.querySelector('input[name="obs-port"]').value || '4444'
+    const password = document.querySelector('input[name="obs-password"]').value
+
+    // 接続
+    socket = new WebSocket(`ws://${ipaddr}:${port}`)
+
+    socket.addEventListener('error', function (error) {
+      console.log('[ERROR] ', error);
+    })
+
+    // ソケットが開いたら認証開始
+    socket.addEventListener('open', function (event) {
+      status.textContent = 'is OPEN';
+      socket.send(JSON.stringify({
+        'request-type': 'GetAuthRequired',
+        'message-id': 'auth-req1'
+      }));
+    });
+    
+    // OBSからの打ち返し
+    socket.addEventListener('message', function (event) {
+      const msg =  JSON.parse(event.data);
+      console.log('[Message] ', msg);
+
+      switch (msg['message-id']) {
+        case 'auth-req1':
+          // 認証いらない系
+          if (!msg['authRequired']) {
+            status.textContent = 'is CONNECTED';
+            break;
+          }
+          // いる系
+          const encoder = new TextEncoder()
+          // secret_string = password + salt
+          crypto.subtle.digest('SHA-256', encoder.encode(password + msg['salt']))
+            .then((shash) => {
+              const secret = btoa(String.fromCharCode.apply(null, new Uint8Array(shash)))
+              // auth_response_string = secret + challenge
+              crypto.subtle.digest('SHA-256', encoder.encode(secret + msg['challenge']))
+                .then((rhash) => {
+                  const authRes = btoa(String.fromCharCode.apply(null, new Uint8Array(rhash)))
+                  socket.send(JSON.stringify({
+                    'request-type': 'Authenticate',
+                    'message-id': 'auth-req2',
+                    'auth': authRes
+                  }));
+                })
+            })
+            break;
+          case 'auth-req2':
+            if (msg['status'] != 'ok') {
+              status.textContent = 'is [ERROR] ' + msg['error'];
+              break;
+            }
+            status.textContent = 'is CONNECTED';
+            break;
+      }
+    });
+  }
+  
+  // 音声認識のいろいろ
+  document.querySelector('div[name="speech-recognition-submit"]').onclick = function() {
+    AlpacaRecognizer();
+  }
+  document.querySelector('div[name="speech-translate-submit"]').onclick = function() {
+    AlpataTranslate("吾輩はアルパカである。名前はまだない。", () => {});
+  }
+  document.querySelector('div[name="speech-speaker-native-submit"]').onclick = function() {
+    AlpataSpeaks("吾輩はアルパカである。名前はまだない。", 'voice-target-native');
+  }
+  document.querySelector('div[name="speech-speaker-foreign-submit"]').onclick = function() {
+    AlpataSpeaks("I am an alpaca. there is no name yet.", 'voice-target-foreign');
+  }
+}
+
+// 音声認識
+function AlpacaRecognizer() {
+  let recognition = new SpeechRecognition();
+  recognition.continuous = true;
+  recognition.lang = 'ja-JP';
+  recognition.interimResults = true;
+  
+  let detected = false;
+
+  let diagnostic = document.querySelector('div[name="NativeLang"]');
+
+  recognition.onresult = function(event) {
+    let results = event.results;
+    for (let i = event.resultIndex; i < results.length; i++) {
+      diagnostic.textContent = results[i][0].transcript;
+      if (results[i].isFinal) {
+        const text = diagnostic.textContent;
+        console.log('[FINAL] ' + text)
+        toOBS(text, document.querySelector('input[name="obs-text-native-source"]').value || 'native')
+        AlpataSpeaks(text, 'voice-target-native')
+        AlpataTranslate(text, AlpacaRecognizer);
+      }
+    }
+  }
+
+  recognition.onspeechend = function() {
+    AlpacaRecognizer();
+  }
+
+  recognition.onnomatch = function(event) {
+    recognition.abort();
+    AlpacaRecognizer();
+  }
+
+  recognition.onerror = function(event) {
+    recognition.abort();
+    AlpacaRecognizer();
+  }
+
+  try {
+    recognition.start();
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function AlpataTranslate(text, nextFunc) {
+  const output = document.querySelector('div[name="ForeignLang"]')
+  // 翻訳情報取得
+  const apikey = document.querySelector('input[name="gas-deploy-key"]').value || 'AKfycbx76Gd_ytJJxInNVqVMUhEXpzEL1zsZpb_vRw-Z7S3ZR6n-5dM'
+  const source = document.querySelector('input[name="gas-source"]').value || 'ja'
+  const target = document.querySelector('input[name="gas-target"]').value || 'en'
+  if (text === "") {
+    return;
+  }
+  // 翻訳する
+  console.log(
+    `https://script.google.com/macros/s/${apikey}/exec?text=${(text)}&source=${source}&target=${target}`)
+  $.ajax({
+    url: `https://script.google.com/macros/s/${apikey}/exec?text=${(text)}&source=${source}&target=${target}`,
+    dataType: "jsonp",
+    jsonpCallback: "test",
+    timeout: 10000
+  }).done(function(data) {
+    const translated = data["translated"]
+    output.textContent = translated;
+    toOBS(translated, document.querySelector('input[name="obs-text-foreign-source"]').value || 'foreign')
+    AlpataSpeaks(translated, 'voice-target-foreign')
+    nextFunc();
+  })
+  .fail(function(data) {
+    output.textContent = "[ERROR] " + data;
+    nextFunc();
+  });
+}
+
+function AlpataSpeaks(text, targetName) {
+  const selectbox = document.querySelector(`select[name="${targetName}"]`)
+  const utter = new SpeechSynthesisUtterance(text);
+  utter.volume = document.querySelector(`input[name="${targetName}-volume"]`).value
+  utter.pitch = document.querySelector(`input[name="${targetName}-pitch"]`).value
+  utter.rate = document.querySelector(`input[name="${targetName}-rate"]`).value
+  utter.voice = speechSynthesis.getVoices()[selectbox.selectedIndex];
+  speechSynthesis.speak(utter);
+}
+
+function toOBS(text, sourceName) {
+  if (socket == null || socket.readyState != 1) {
+    console.log('websocket is not ready.');
+    return;
+  }
+  socket.send(JSON.stringify({
+    'request-type': 'SetTextGDIPlusProperties',
+    'message-id': 'settextgdi-req',
+    'source': sourceName,
+    'text': text
+  }));
+}
